95446fb69c5d944123d8e3434df23663113e9ff7
Justin Walrath <walrathjaw@gmail.com>
2015-03-16 23:37:17 -0500

    Added server reservation.

--- data/ServerDao.py
+++ data/ServerDao.py
@@ -38,3 +38,29 @@ class ServerDao:
                 return (list(map(lambda x: Server(x[0], x[1], x[2], app_id), server_rows)), None)
         except Exception as er:
             return (None, "There was a db issue: " + str(er))
+    
+    """
+        Find x number of available servers or all that are available.
+    """
+    def find_available_servers(self, quantity):
+        try:
+            with self.conn:
+                cur = self.conn.cursor()
+                cur.execute("SELECT id, host, url from servers where app_id = null limit = ?", (quantity,))
+                server_rows = cur.fetchall()
+                return (list(map(lambda x: Server(x[0], x[1], x[2], None), server_rows)), None)
+        except Exception as er:
+            return (None, "There was a db issue: " + str(er))
+        
+    """
+        Tie an app to a number of servers.
+    """
+    def tie_app_to_servers(self, app_id, available_servers):
+        try:
+            with self.conn:
+                cur = self.conn.cursor()
+                server_id_string = ', '.join("?" * available_servers)
+                cur.execute("update servers set app_id = ? where id in {0}".format(server_id_string), tuple([app_id] + available_servers))
+                return (None, "ok")
+        except Exception as er:
+            return (None, "There was a db issue: " + str(er))
